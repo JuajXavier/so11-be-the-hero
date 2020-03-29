47b3994782e6282145ff18b9d5936a3aa8a1c821
João Xavier <jvitorxavier@gmail.com>
2020-03-28 22:59:09 -0300

    Back-end da aplicação

---
+++ backend/src/database/migrations/20200327142645_create_ongs.js
@@ -0,0 +1,19 @@
+
+// método UP para criação da tabela.
+
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function (table) {
+      table.string('id').primary();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('whatsapp').notNullable();
+      table.string('city').notNullable();
+      table.string('uf', 2).notNullable();
+  });
+};
+
+// método down é pra caso precise voltar atrás/deletar. Resolver problemas
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
